f9c8caf4c53fb80d8a66f3787f8f20cf88a791dc
Kiran Kumar Byala <kirankiran81174@gmail.com>
2025-06-10 22:41:04 +0530

    Add files via upload

---
+++ bubblesortEx1.java
@@ -0,0 +1,26 @@
+package kiran;
+
+import java.util.Arrays;
+
+public class bubblesortEx1 {
+
+	public static void main(String[] args) {
+		int[]numbers= {5,3,8,4,2};
+		System.out.println("before sorting:");
+		System.out.println(Arrays.toString(numbers));
+		System.out.println(numbers.length-1);
+		for(int i=0;i<numbers.length-1;i++) {
+			for(int j=0;j<numbers.length-1;j++) {
+				if(numbers[j]>numbers[j+1]) {
+					int temp=numbers[j];
+					numbers[j]=numbers[j+1];
+					numbers[j+1]=temp;
+				}
+			}
+		}
+		System.out.println("Bubble Sorted List");
+		for(int num:numbers) {
+			System.out.print(num+" ");
+		}
+	}
+}
